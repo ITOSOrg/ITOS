a86dda5d2b94fe926d4a64443078ba97a993e620
vaishalikhandekar <Vaishali.Khandekar@yahoo.com>
2015-11-17 18:12:18 -0600

    added fields

--- components/profile/data/demo/EmailAddressLink.sql
+++ components/profile/data/demo/EmailAddressLink.sql
@@ -5,5 +5,5 @@
 --  DDL for Table EmailAddressLink
 --------------------------------------------------------
 
---INSERT INTO EmailAddressLink(emailAddressLinkID ,	relatedID ,emailAddressID ,	typeCode ,	primaryInd ,	startDate ,							endDate ,	comments ,		recordStatus ,versionNo)
---                VALUES( 401,						101,			NULL,			NULL,		'True'			to_date('01-aug-13', 'DD-MON-RR'),	NULL,		'Comment',		'True',			1);
+INSERT INTO EmailAddressLink(emailAddressLinkID ,	relatedID ,		emailAddressID ,		typeCode ,		primaryInd ,	startDate ,								endDate ,	comments ,		recordStatus ,	versionNo)
+               VALUES( 			401,				101,			701,			'primary',		'True',			to_date('01-aug-13', 'DD-MON-RR'),		NULL,		'Comment',		'Active',				1);
--- components/profile/data/demo/PhoneNumberLink.sql
+++ components/profile/data/demo/PhoneNumberLink.sql
@@ -5,5 +5,5 @@
 --  DDL for Table PhoneNumberLink
 --------------------------------------------------------
 
---INSERT INTO PhoneNumberLink(phoneNumberLinkID,relatedID ,phoneNumberID ,typeCode ,	primaryInd ,startDate ,							endDate ,comments ,			recordStatus ,versionNo)
- --             VALUES(			601,			NULL,		501,			NULL,	'True',		to_date('01-aug-11', 'DD-MON-RR'),		NULL,	'Comments',			'True',			1);			   
+INSERT INTO PhoneNumberLink(phoneNumberLinkID,	relatedID ,	phoneNumberID ,	typeCode ,		primaryInd ,	startDate ,								endDate ,	comments ,			recordStatus ,		versionNo)
+             VALUES(			601,			101,		501,			'primary',		'True',			to_date('01-aug-11', 'DD-MON-RR'),		NULL,		'Comments',			'Active',				1);			   

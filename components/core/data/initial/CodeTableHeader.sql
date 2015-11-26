dd79157bbef852bde3e02142af50952061ae39ae
vaishalikhandekar <Vaishali.Khandekar@yahoo.com>
2015-11-26 15:32:23 -0600

    inserted data in codetable and codeheader table

---
+++ components/core/data/initial/CodeTableHeader.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------
+--  File created - 12/30/2014   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table CodeTableHeader
+--------------------------------------------------------
+
+
+
+
+
+INSERT INTO CodeTableHeader (tableName,	   defaultCode,	 parentCodeTable, comments,  recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',		'ST0001',	       null,	      null,	    'Active',	   'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',             1);
+				
+	
+	INSERT INTO CodeTableHeader (tableName,	   defaultCode,	 parentCodeTable, comments,  recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('Gender',		  'GEN0001',	       null,	      null,	    'Active',	   'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',             1);
+								
---
+++ components/core/data/initial/CodeTableItem.sql
@@ -0,0 +1,40 @@
+--------------------------------------------------------
+--  File created - 12/30/2014   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table CodeTableItem
+--------------------------------------------------------
+
+
+
+
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',	 'ST0001',      'New York',	   'is a State',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+
+
+					 
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',	 'ST0002',   'New Jersy',	  'It is a State',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+
+
+					 
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',	 'ST0003',  'Washington DC',  'It is a State',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+				
+
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',	 'ST0004',   'Chicago',	      'It is a State',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+				
+
+				
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('State',	 'ST0005',      'Maryland',       'Is a State',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+
+
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('Gender',	 'GEN0001',      'Male',       'Is a Gender',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+	
+	
+INSERT INTO CodeTableItem (tableName,	   code,	 description,       annotation,        isEnabled, 	sortorder,	 parentCode,   	comments,  		recordStatus,	  createdBy,	        createdOn,	                          lastModifiedBy,  		    lastModifiedOn, 				versionNo) 
+				     VALUES ('Gender',	 'GEN0002',      'Female',       'Is a Gender',	   1,   	null,	    	null,		'comment',		'Active',	   		'Rahul',	    '26-NOV-15 02.48.50.000000000 AM',                'Rahul',		 '26-NOV-15 02.48.50.000000000 AM',         1);
+				

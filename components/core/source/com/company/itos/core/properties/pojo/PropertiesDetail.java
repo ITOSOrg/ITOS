d23ada467199425b1c442a3cc030a3f9a86ffec3
vaishalikhandekar <Vaishali.Khandekar@yahoo.com>
2015-12-12 15:20:01 -0600

    update worked

--- components/core/source/com/company/itos/core/properties/pojo/PropertiesDetail.java
+++ components/core/source/com/company/itos/core/properties/pojo/PropertiesDetail.java
@@ -26,6 +26,22 @@ public class PropertiesDetail {
 
 	private Timestamp lastModifiedOn;
 	
+	private String updateAction = "update";
+	
+	/**
+	 * @return the updateAction
+	 */
+	public String getUpdateAction() {
+		return updateAction;
+	}
+
+	/**
+	 * @param updateAction the updateAction to set
+	 */
+	public void setUpdateAction(String updateAction) {
+		this.updateAction = "update";
+	}
+
 	public String getCreatedBy() {
 		return createdBy;
 	}
